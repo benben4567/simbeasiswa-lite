3e7ae5e48bf6ea7677bd469aeec0f64b2e32ce07
benben4567 <benben4567@gmail.com>
2021-04-29 21:30:57 +0700

    passing data to view

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Major;
+use App\Models\ScholarshipType;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index()
     {
-      return view('pages.pelaporan.index');
+      $majors = Major::all();
+      $types = ScholarshipType::all();
+      return view('pages.pelaporan.index', compact('majors', 'types'));
     }
 }
