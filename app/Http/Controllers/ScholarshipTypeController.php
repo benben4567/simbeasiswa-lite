2756889be3d813ab51ea9e3d72a9a5c9b44ca862
benben4567 <benben4567@gmail.com>
2021-05-01 12:12:57 +0700

    feat: add import method

--- app/Http/Controllers/ScholarshipTypeController.php
+++ app/Http/Controllers/ScholarshipTypeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\ScholarshipTypesImport;
 use App\Models\ScholarshipType;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ScholarshipTypeController extends Controller
 {
@@ -36,6 +38,18 @@ class ScholarshipTypeController extends Controller
       $type = ScholarshipType::find($request->id)->update($request->all());
       if ($type) {
         return redirect()->back()->with('success', 'Data berhasil disimpan.');
+      }
+    }
+
+    public function import(Request $request)
+    {
+      $request->validate([
+        'file' => 'required|mimes:xls,xlsx'
+      ]);
+
+      $import = Excel::import(new ScholarshipTypesImport, $request->file);
+      if ($import) {
+        return redirect()->back()->with('success', 'Data berhasil diimport');
       }
     }
 }
